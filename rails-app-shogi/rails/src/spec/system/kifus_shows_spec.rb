0d3bc186b2a9b149a6ed525e730a27a7ae25e00b
ynaoy <naozou22113@yahoo.co.jp>
2022-05-06 14:38:30 +0900

    kifus_helper内のバグを修正。kifus/showのテストを大幅に追加予定

--- rails-app-shogi/rails/src/spec/system/kifus_shows_spec.rb
+++ rails-app-shogi/rails/src/spec/system/kifus_shows_spec.rb
@@ -11,17 +11,6 @@ RSpec.describe "KifusShows", type: :system do
 
   describe "Kifu_Test" do
 
-    it "display normaly",js:true do
-      visit kifu_path(id:@kifu.id)
-
-      text = ["飛","角","金","銀","桂","香","歩","王"]
-      for t in text do
-        expect(page).to have_content(t)
-      end
-
-      expect(page).to have_selector("#player")
-    end
-
     it "change state" , js: true do
 
       visit kifu_path(id:@kifu.id)
@@ -41,13 +30,19 @@ RSpec.describe "KifusShows", type: :system do
       find("#next_10").click
       expect(page).to have_selector("#state", text: 10)
       #state==max_stateだと次へを押しても値が変わらない
+      2.times do
+        find("#next_10").click
+      end
       find("#next_1").click
-      expect(page).to have_selector("#state", text: 10)
+      expect(page).to have_selector("#state", text: 30)
 
-      #前へ×10を押すとstateが10→0になる
+      #前へ×10を押すとstateが30→20になる
       find("#back_10").click
-      expect(page).to have_selector("#state", text: 0)
+      expect(page).to have_selector("#state", text: 20)
       #state==0だと前へを押しても値が変わらない
+      2.times do
+        find("#back_10").click
+      end
       find("#back_1").click
       expect(page).to have_selector("#state", text: 0)
 
@@ -59,6 +54,18 @@ RSpec.describe "KifusShows", type: :system do
       expect(page).to have_selector("#tags")
       #tagの中身が存在するか確認
       expect(page).to have_content(@tag1.name)
+    end
+
+    it "display normaly",js:true do
+      visit kifu_path(id:@kifu.id)
+
+      expect(page).to have_selector("#player")
+      text = ["飛","角","王","金","銀","桂","香"]
+      for i in 0..(text.length-1) do
+        (i<3)? num=2: num=4
+        expect(page).to have_content(text[i],count:num)
+      end
+      expect(page).to have_content("歩", count: 18)
     end
 
   end
