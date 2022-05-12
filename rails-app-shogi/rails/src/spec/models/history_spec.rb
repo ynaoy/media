fbfb2a22da3377e5cf21c2f958c61eba81716ca5
ynaoy <naozou22113@yahoo.co.jp>
2022-05-12 13:16:57 +0900

    spec/models/history_spec.rbにテストを追加

--- rails-app-shogi/rails/src/spec/models/history_spec.rb
+++ rails-app-shogi/rails/src/spec/models/history_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe History, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = FactoryBot.create(:user,name:"Sample_user")
+    @history = @user.histories.build(kifu_id:1)
+  end
+
+  it "should be valid" do
+    expect(@history).to be_valid
+  end
+
+  it "user_id should not presence" do
+    @history.user_id = nil
+    expect(@history).not_to be_valid
+  end
+
+  it "kifu_id should not be presence" do
+    @history.kifu_id = nil
+    expect(@history).not_to be_valid
+  end
+
 end
