3cb798df1008adfabccd6106e27b88ac697fdec7
ynaoy <naozou22113@yahoo.co.jp>
2022-05-18 18:20:36 +0900

    Favoriteモデルとそのmigrationファイル、テストを追加。app/models/user.rbを修正。db以下seeds.rbとschema.rbを修正

---
+++ rails-app-shogi/rails/src/spec/models/favorite_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  before do
+    @user = FactoryBot.create(:user,name:"Sample_user")
+    @kifu = FactoryBot.create(:kifu,user_id:@user.id)
+    @favorite = FactoryBot.create(:favorite,
+                                  user_id:@user.id,
+                                  kifu_id:@kifu.id)
+    
+  end
+
+  it "should be valid" do
+    expect(@favorite).to be_valid
+  end
+
+  it "user_id should not presence" do
+    @favorite.user_id = nil
+    expect(@favorite).not_to be_valid
+  end
+
+  it "kifu_id should not be presence" do
+    @favorite.kifu_id = nil
+    expect(@favorite).not_to be_valid
+  end
+
+  it "Favorite.favorite_kifus should work" do
+    favorites = Favorite.favorite_kifus(@user.id)
+    expect(favorites).not_to be_nil
+    expect(favorites).not_to be_empty
+
+    favorite = favorites.first
+    expect(favorite.id).to be @kifu.id
+    expect(favorite.created_at).to eq @kifu.created_at
+    expect(favorite.watch_at).to eq @favorite.created_at
+  end
+end
+
