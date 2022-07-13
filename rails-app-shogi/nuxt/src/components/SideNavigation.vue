ee5b1b159d838e974dd4d6d5cdbc4841845b6f20
ynaoy <naozou22113@yahoo.co.jp>
2022-07-13 09:13:29 +0900

    nuxt側でsidenaviSideNavigationコンポーネントに/users/[id]へのリンクを設定した

--- rails-app-shogi/nuxt/src/components/SideNavigation.vue
+++ rails-app-shogi/nuxt/src/components/SideNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="list-group list-group-flush">
-    <nuxt-link v-if="loginFlg" to="/" :class="class_name">
+    <nuxt-link v-if="loginFlg" :to = "`/users/${user_id}`" :class="class_name">
       マイページ
     </nuxt-link>
 
@@ -8,7 +8,7 @@
       お気に入り
     </nuxt-link>
     
-    <nuxt-link v-if="loginFlg" to="/kifus/new" :class="class_name">
+    <nuxt-link v-if="loginFlg" to = "/kifus/new" :class="class_name">
       棋譜を追加
     </nuxt-link>
     
@@ -31,6 +31,7 @@
 <script setup>
 
   //親コンポーネントから貰う奴ら
+  const user_id = inject('user_id')
   const loginFlg = ref(inject('loginFlg'))
 
   const tags = ["相掛かり","矢倉","角換わり","横歩取り","三間飛車","四間飛車","角交換四間飛車","角交換中飛車"]
