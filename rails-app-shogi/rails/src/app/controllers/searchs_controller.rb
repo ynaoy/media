c012d03ab09b23104221bfd1a67541a9a26f4818
ynaoy <naozou22113@yahoo.co.jp>
2022-07-18 16:32:12 +0900

    rails側でsearchsコントローラーのsearchメソッドにjson形式のレスポンスを追加

--- rails-app-shogi/rails/src/app/controllers/searchs_controller.rb
+++ rails-app-shogi/rails/src/app/controllers/searchs_controller.rb
@@ -3,6 +3,13 @@ class SearchsController < ApplicationController
   def search
     @users = User.search_user("name",search_params[:query])
     @kifus = Kifu.search_kifu_and_tag("player1 OR player2",search_params[:query]).order(id: "desc").page(params[:page]).per(20)
+    respond_to do |format|
+      format.html { render "search"}
+      format.json { render json: {  
+                      users: @users.to_json(only: %i[ id name ]),
+                      kifus: @kifus.to_json(only: %i[ id user_id title player1 player2 win created_at ]) }
+                  }
+    end
   end
 
   private
