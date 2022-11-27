553f9f9460f36c7b7a5e1cc4a7b4732a9c9f4bb3
ynaoy <naozou22113@yahoo.co.jp>
2022-11-27 14:57:19 +0900

    nuxt側でkifu-kentoコンポーネントから「ソフトの読み筋を表示」ボタンを削除して論理的な部分を抜本的に変更。そのテストを修正

--- rails-app-shogi/nuxt/src/components/kifus/kifu-kento.vue
+++ rails-app-shogi/nuxt/src/components/kifus/kifu-kento.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
-    <div v-if= "kento_clicked == false" >
-      <button v-if= "is_kento(kento)" v-on:click="show_kento()" id="kento_button"> 
-        ソフトの読み筋を表示 
-      </button>
-      <button v-else-if= "my_kifu"> 
-        ソフトに自動検討させる
-      </button>
+    <div v-if="is_kento(kento)" id="kento_result"> ソフトの読み筋表示してます </div>
+    <div v-else-if="my_kifu">
+      <div v-if="kento=='processing_now'">  検討中... </div>
+      <button v-else > ソフトに自動検討させる </button>
     </div>
-    <div v-else id="kento_result"> ソフトの読み筋表示してます </div>
   </div>
 </template>
 
@@ -19,21 +15,16 @@
   const kento     = inject('kento')
   console.log(my_kifu)
   console.log(kento)
-  //このコンポーネントで使う変数群
-  const kento_clicked = ref(false)
   
   //子コンポーネントに渡す変数群
   provide('kento', kento)
 
   //このコンポーネントで使うメソッド群
 
-  //kento変数がnullならfalse、そうでないならtrueを返す
-  const is_kento = (item)=>{ return item != null }
-  
-  //kentoボタンがクリックされたらフラグを更新する
-  const show_kento = ()=>{ kento_clicked.value = !kento_clicked.value }
+  //kento変数がnullかprocessing_nowならfalse、そうでないならtrueを返す
+  const is_kento = (item)=>{ return !((item == null)||(item=='processing_now'))}
 
-  defineExpose({ kento, kento_clicked, is_kento, show_kento })
+  defineExpose({ kento, my_kifu, is_kento})
 </script>
 
 <style lang="scss" scoped>
--- rails-app-shogi/nuxt/src/test/Component/kifu-kentoTest.spec.ts
+++ rails-app-shogi/nuxt/src/test/Component/kifu-kentoTest.spec.ts
@@ -8,45 +8,55 @@ describe("kifu-kento test", async() => {
   const { Mount } = MountHelper()
   const wrapper = Mount(KifuKento,{ kento:"Test kento", my_kifu:true })
 
-  //show_kento関数が呼び出されているかのチェック用
-  const spy = vi.spyOn(wrapper.vm,"show_kento")
-  await wrapper.vm.$forceUpdate()
 
-  describe("kento === nullの時", async() => {
-    it("my_kifu == trueの時、正しく表示されているか", async() => {
+  describe("my_kifu == trueの時", async() => {
+    it("kento == processing_nowの時、「検討中...」が表示されている", async() => {
+      // このテスト用のコンポーネントをマウント
+      const wrapper_pnow = Mount(KifuKento,{ kento: "processing_now", my_kifu:true })
+      expect(wrapper_pnow.text()).toContain("検討中...")
+      expect(wrapper_pnow.text()).not.toContain("ソフトに自動検討させる")
+      expect(wrapper_pnow.text()).not.toContain("ソフトの読み筋表示してます")
+    })
+
+    it("kento == nullの時、「ソフトに自動検討させる」が表示されている", async() => {
       // このテスト用のコンポーネントをマウント
       const wrapper_null = Mount(KifuKento,{ kento: null, my_kifu:true })
 
       expect(wrapper_null.text()).toContain("ソフトに自動検討させる")
-      expect(wrapper_null.text()).not.toContain("ソフトの読み筋を表示")
+      expect(wrapper_null.text()).not.toContain("検討中...")
+      expect(wrapper_null.text()).not.toContain("ソフトの読み筋表示してます")
     })
 
-    it("my_kifu === falseの時、正しく表示されているか", async() => {
-      // このテスト用のコンポーネントをマウント
-      const wrapper_null = Mount(KifuKento,{ kento: null, my_kifu:false })
-
-      expect(wrapper_null.text()).not.toContain("ソフトに自動検討させる")
-      expect(wrapper_null.text()).not.toContain("ソフトの読み筋を表示")
+    it("それ以外の時、「ソフトの読み筋を表示」が表示されている", async() => {
+      expect(wrapper.text()).toContain("ソフトの読み筋表示してます")
+      expect(wrapper.text()).not.toContain("ソフトに自動検討させる")
+      expect(wrapper.text()).not.toContain("検討中...")
     })
   })
   
-  describe("kento != nullの時", async() => {
-
-    it("正しく表示されているか", async() => {
-      expect(wrapper.text()).toContain("ソフトの読み筋を表示")
-      expect(wrapper.text()).not.toContain("ソフトに自動検討させる")
+  describe("my_kifu == falseの時", async() => {
+    it("kento == processing_nowの時、すべて表示されていない", async() => {
+      // このテスト用のコンポーネントをマウント
+      const wrapper_pnow = Mount(KifuKento,{ kento: "processing_now", my_kifu:false })
+      expect(wrapper_pnow.text()).not.toContain("ソフトの読み筋表示してます")
+      expect(wrapper_pnow.text()).not.toContain("ソフトに自動検討させる")
+      expect(wrapper_pnow.text()).not.toContain("検討中...")
     })
 
-    it("ソフトの読み筋を表示ボタンをクリック後、正しく表示されているか", async() => {
-      //#kento_resultボタンをクリックしてコンポーネント内の変数を更新
-      wrapper.find('#kento_button').trigger('click')
-      await wrapper.vm.$nextTick()
-      console.log(wrapper.vm)
-      //show_kento関数が呼び出されているかチェック
-      expect(spy).toHaveBeenCalled()
-  
-      expect(wrapper.text()).toContain("ソフトの読み筋表示してます")
-      expect(wrapper.text()).not.toContain("ソフトの読み筋を表示")
+    it("kento == nullの時、すべて表示されていない", async() => {
+      // このテスト用のコンポーネントをマウント
+      const wrapper_null = Mount(KifuKento,{ kento: null, my_kifu:false })
+      expect(wrapper_null.text()).not.toContain("ソフトの読み筋表示してます")
+      expect(wrapper_null.text()).not.toContain("ソフトに自動検討させる")
+      expect(wrapper_null.text()).not.toContain("検討中...")
+    })
+
+    it("それ以外の時、「ソフトの読み筋を表示」が表示されている", async() => {
+      // このテスト用のコンポーネントをマウント
+      const wrapper_disp = Mount(KifuKento,{ kento: "Test kento", my_kifu:false })
+      expect(wrapper_disp.text()).toContain("ソフトの読み筋表示してます")
+      expect(wrapper_disp.text()).not.toContain("ソフトに自動検討させる")
+      expect(wrapper_disp.text()).not.toContain("検討中...")
     })
   })
 
