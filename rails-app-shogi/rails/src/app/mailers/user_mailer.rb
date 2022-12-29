9315a32fff7c095d396618fe0717d967794fd65d
ynaoy <naozou22113@yahoo.co.jp>
2022-12-29 21:37:23 +0900

    rails側でaccount_activationメーラーのviewを変更。そのテストを修正

--- rails-app-shogi/rails/src/app/mailers/user_mailer.rb
+++ rails-app-shogi/rails/src/app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "将棋のお時間の認証コードは#{@user.activation_token}です"
   end
 
 end
