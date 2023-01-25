aa7a3ba7c0ff1f2a9d5424a8f67de9420b874252
ynaoy <naozou22113@yahoo.co.jp>
2023-01-25 09:31:48 +0900

    nuxt側でcomposables/ValidationHelper.tsにvalid_passwordメソッドを追加。そのテストを追加

--- rails-app-shogi/nuxt/src/composables/ValidationHelper.ts
+++ rails-app-shogi/nuxt/src/composables/ValidationHelper.ts
@@ -9,8 +9,25 @@ export const ValidationHelper = () =>{
 
   const reset_validation = ()=>{ validation.value = "" }
 
+  // パスワードが不正ならバリデーションを表示してfalseを返す。そうでなければtrueを返す
+  const valid_password = (password:string, password_confirmation:string) =>{
+    if(password != password_confirmation){
+      set_validation("パスワードとパスワードの確認が一致しません")
+      return false
+    }
+
+    else if(password.length < 8){
+      set_validation("パスワードが短すぎます。8文字以上にしてください")
+      return false
+    }
+
+    else{ 
+      return true 
+    }
+  }
   return {  get_validation: get_validation,
             set_validation: set_validation,
             reset_validation: reset_validation,
+            valid_password: valid_password,
           }
 }
--- rails-app-shogi/nuxt/src/test/Unit/ValidationHelperTest.spec.ts
+++ rails-app-shogi/nuxt/src/test/Unit/ValidationHelperTest.spec.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
 import { ValidationHelper } from "../../composables/ValidationHelper";
 
 describe("ValidationHelper test", async() => {
   
   //このテストでチェックするやつら
-  const { get_validation, set_validation, reset_validation, } = ValidationHelper()
+  const { get_validation, set_validation, reset_validation, valid_password } = ValidationHelper()
 
   describe("get_validationメソッド",()=>{
     it("正しく動作するかチェック", async() => {
@@ -25,6 +25,30 @@ describe("ValidationHelper test", async() => {
       set_validation("test")
       expect(get_validation()).toBe("test")
       reset_validation()
+      expect(get_validation()).toBe("")
+    })
+  })
+
+  describe("valid_passwordメソッド",()=>{
+    afterEach(()=>{ 
+      reset_validation()
+    })
+    
+    it("パスワードとパスワードの確認が一致しない時、バリデーションが表示されfalseが返る", async() => {
+      let ret = valid_password("password", "false_password")
+      expect(ret).toBe(false)
+      expect(get_validation()).toBe("パスワードとパスワードの確認が一致しません")
+    })
+
+    it("パスワードが8文字より短い時、バリデーションが表示されfalseが返る", async() => {
+      let ret = valid_password("ppppppp", "ppppppp")
+      expect(ret).toBe(false)
+      expect(get_validation()).toBe("パスワードが短すぎます。8文字以上にしてください")
+    })
+
+    it("パスワードに不正がない時、バリデーションが表示されずtrueが返る", async() => {
+      let ret = valid_password("password", "password")
+      expect(ret).toBe(true)
       expect(get_validation()).toBe("")
     })
   })
