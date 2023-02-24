ab5800db84091204fbd180de68a20511ec2dcca0
ynaoy <naozou22113@yahoo.co.jp>
2023-02-24 23:37:35 +0900

    nuxt側でKifuIndexコンポーネントの使用言語をtypescriptにして型を付けた

--- rails-app-shogi/nuxt/src/components/KifuIndex.vue
+++ rails-app-shogi/nuxt/src/components/KifuIndex.vue
@@ -2,15 +2,15 @@
   <KifusItems :kifus="kifus"/>
 </template>
 
-<script setup>
+<script lang="ts" setup>
   import KifusItems from './kifus/kifus-items.vue'
   import { SessionHelper } from '../composables/SessionHelper'
   
   // 親コンポーネントから貰う奴ら。
-  const loginFlg = inject('loginFlg')
-  const user_id = inject('user_id')
-  const csrf_token = inject('csrf_token')
-  const { kifus } = defineProps(['kifus'])
+  const loginFlg :boolean =   inject('loginFlg')
+  const csrf_token :string =  inject('csrf_token')
+  const user_id :number =     inject('user_id')
+  const { kifus } = defineProps<{ kifus: [{ [key:string]: any }]}>()
 
   // 子コンポーネントに渡す奴ら。
   provide('user_id',user_id)
