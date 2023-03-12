91b5901929806e3695cad518a53f13eadc97c934
ynaoy <naozou22113@yahoo.co.jp>
2023-03-13 04:03:20 +0900

    nuxt側でcomposables/subBoardObject.tsの関数に型情報を追加した。

--- rails-app-shogi/nuxt/src/composables/kifus/subBoardObject.ts
+++ rails-app-shogi/nuxt/src/composables/kifus/subBoardObject.ts
@@ -1,12 +1,14 @@
-import { reactive } from 'vue'
+import { reactive,Ref } from 'vue'
 
-export const subBoardObject = (sub_board_text, sub_board_num, sub_board_id)=>{
+export const subBoardObject = ( sub_board_text: Ref<string[][]>,
+                                sub_board_num: Ref<number[][]>, 
+                                sub_board_id: number)=>{
 
   //リアクティブな変数群
   const sub_board_states =reactive({ })
 
   //メソッド群
-  const process_num = function(num){
+  const process_num = <T>(num:T):string|void =>{
     if(num!="") return "×"+num
   }
 
