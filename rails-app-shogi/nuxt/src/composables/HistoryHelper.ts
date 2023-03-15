ad784eba2eabb43e1f76c3a570c75a44047bb806
ynaoy <naozou22113@yahoo.co.jp>
2023-03-15 11:11:47 +0900

    nuxt側でcomposables/HistoryHelper.tsの関数に型情報を追加した。

--- rails-app-shogi/nuxt/src/composables/HistoryHelper.ts
+++ rails-app-shogi/nuxt/src/composables/HistoryHelper.ts
@@ -5,13 +5,18 @@ export const HistoryHelper = ()=>{
   // 使うヘルパー
   const { timewithzone_to_str } = AppHelper()
 
-  // 管理する変数群
-  const all_day = new Set(), watched_ids = new Set()
-  let today
+  // ---変数群---
+
+  // 表示されたすべての日付
+  const all_day:Set<string> = new Set()
+  // 同一日の重複した履歴を表示しないためにこの変数で管理する
+  const watched_ids:Set<number> = new Set()
+  // 現在の日付
+  let today:Date
 
   // メソッド
   // 日付が今日か昨日なら、表示する値を変える。
-  const display_day = ((day)=>{
+  const display_day = ((day:string|number|Date):string =>{
 
     day = timewithzone_to_str(day)
     step_day(day) // 日付が変わった時の処理
@@ -27,18 +32,18 @@ export const HistoryHelper = ()=>{
   })
   
   // 表示する日付が変わった時の処理
-  const step_day = (day)=>{
+  const step_day = (day:string):void =>{
     all_day.add(day)    // 既に表示した日付を追加
     watched_ids.clear() // 同一日の重複した履歴を再度管理するため、日付が変わったら初期化する
   }
   
   // 日付を表示するかどうかのフラグ。重複した日付を表示させない
-  const display_flg = (day)=>{
+  const display_flg = (day:string|number|Date):boolean =>{
     return !all_day.has(timewithzone_to_str(day))
   }
   
   // 履歴の中身を表示するかどうかのフラグ。同一日の重複した履歴は一度しか表示させない
-  const kifu_flg = (id)=>{
+  const kifu_flg = (id:number):boolean=>{
     if(!watched_ids.has(id)){
       watched_ids.add(id)
       return true // 表示させる
@@ -47,7 +52,7 @@ export const HistoryHelper = ()=>{
   }
   
   // 現在の日付を返す
-  const get_now = ()=>{
+  const get_now = ():Date =>{
     let datetime = new Date()
     datetime.setTime(datetime.getTime()+1000*60*60*9) // 日本時間jstに変換
     return datetime
