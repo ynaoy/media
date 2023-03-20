4ff0d8ae2c3306e1f87c331283de9e0557448d08
ynaoy <naozou22113@yahoo.co.jp>
2023-03-20 11:22:04 +0900

    nuxt側でcomposables/SearchHelper.tsの関数に型情報を追加した。

--- rails-app-shogi/nuxt/src/composables/SearchHelper.ts
+++ rails-app-shogi/nuxt/src/composables/SearchHelper.ts
@@ -7,9 +7,9 @@ export const SearchHelper = () => {
   //サーバーサイドのSearchURLにparams付きでGETリクエストを送る。
   //usersとkifusが入った連想配列が帰ってくれば成功。さもなくばエラーを吐き出す
   const search_user_and_kifu = 
-    async function( params:{ query: String},
-                    headers:{},                      
-                  ){
+    async ( params:{ query: String},
+            headers:{[key:string]:any},                      
+          ):Promise<{[key:string]:any}>=>{
 
       let ret
       params['format'] = 'json'
@@ -29,8 +29,8 @@ export const SearchHelper = () => {
       return { "search_data": ret }
     }
 
-    // クエリ付きでsearchページに飛ばす。他にもっといいやり方ありそう
-    const move_search =(query)=>{
+    // クエリ付きでsearchページに飛ばす
+    const move_search =(query:string)=>{
       useRouter().push({ path: '/search', query:{ search: query }})
       //location.href = `${location.origin}/search?search=${query}`
     }
